16e7fc9c75e13a817316d5f7e98092cfc84b5721
Renata Teixeira de Jesus <renata.teixeira@sempreceub.com>
2025-05-30 09:22:42 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Error: division by zero.";
+  }
+  return a / b;
+}
+
+console.log("Add:", add(20, 5));
+console.log("Subtract:", subtract(15, 5));
+console.log("Multiply:", multiply(12, 5));
+console.log("Divide:", divide(12, 5));
